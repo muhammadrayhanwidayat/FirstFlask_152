ad822940a700017cb19fc9d944bb50723ecae069
muhammadrayhanwidayat <murayat99@gmail.com>
2024-12-12 21:35:36 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Route for the home page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Route for the about page
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Route for form submission
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    return render_template('response.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
